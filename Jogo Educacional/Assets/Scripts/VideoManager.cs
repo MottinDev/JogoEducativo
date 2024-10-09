7f1b85ff7db6b153d7501bde16c0ee207fb1eef9
Carlos V. B. Dos Santos <121064459+Carlos-v-b-Santos@users.noreply.github.com>
2024-10-09 12:58:35 -0400

    videoManager
    criei um ScriptObject chamado VideoSO para servir como base para todos os clips, ele tem o clip em si e o próximo a ser tocado, ou uma lista dos próximos, caso tenha mais de uma opção a se seguir. O VideoManager foi alterado para comportar as mudanças.
    É necessário refinar o código e deixar ele mais limpo.

--- Jogo Educacional/Assets/Scripts/VideoManager.cs
+++ Jogo Educacional/Assets/Scripts/VideoManager.cs
@@ -4,59 +4,88 @@ using UnityEngine.UI;
 
 public class VideoManager : MonoBehaviour
 {
-    public VideoPlayer videoPlayer;   
-    public VideoClip[] videoClips;     
-    public Button nextButton;          
-    public Button previousButton;      
+    public VideoPlayer videoPlayer;
+    //public VideoClip[] videoClips;     
+    public VideosSO videoClip;//primeiro videoClip da fase
+    public Button nextButton;
+    public Button previousButton;
+    public Button[] optionsButton;
 
-    private int currentClipIndex = 0;  
+    private int currentClipIndex = 0;
 
     void Start()
     {
         // Verifica se há vídeos no array e toca o primeiro
-        if (videoClips.Length > 0)
+        //if (videoClips.Length > 0)
+        if (videoClip != null)
+            {
+                PlayCurrentVideo();
+            }
+
+            // Adiciona listeners para os botões
+            //nextButton.onClick.AddListener(PlayNextVideo());
+            //previousButton.onClick.AddListener(PlayPreviousVideo);
+
+
+            if (videoClip.options.Count > 1)
+            {
+                nextButton.enabled = false;
+                previousButton.enabled = false;
+                for (int i = 0; i < videoClip.options.Count; i++)
+                {
+                    optionsButton[i].enabled = true;
+                }
+            }
+
+            else
+            {
+                nextButton.enabled = true;
+                previousButton.enabled = true;
+                for (int i = 0; i < videoClip.options.Count; i++)
+                {
+                    optionsButton[i].enabled = false;
+                }
+            }
+        }
+
+        // Toca o vídeo atual com base no índice
+        void PlayCurrentVideo()
         {
+            //if (currentClipIndex < videoClips.Length && currentClipIndex >= 0)
+            //{
+            //    videoPlayer.clip = videoClips[currentClipIndex].videoClip;
+            //    videoPlayer.Play();
+            //}
+            //else
+            //{
+            //    Debug.Log("Índice fora do limite do array de vídeos.");
+            //}
+
+            videoPlayer.clip = videoClip.videoClip;
+        }
+
+        // Troca para o próximo vídeo
+        public void PlayNextVideo(int clipOption = 0)
+        {
+            //currentClipIndex++;
+            //if (currentClipIndex >= videoClips.Length)
+            //{
+            //    currentClipIndex = 0; // Reinicia ao primeiro vídeo se passar do último
+            //}
+            videoClip = videoClip.options[clipOption];
+            videoPlayer.clip = videoClip.videoClip;
+
             PlayCurrentVideo();
         }
 
-        // Adiciona listeners para os botões
-        nextButton.onClick.AddListener(PlayNextVideo);
-        previousButton.onClick.AddListener(PlayPreviousVideo);
+        // Troca para o vídeo anterior
+        //public void PlayPreviousVideo()
+        //{
+        //    currentClipIndex--;
+        //    if (currentClipIndex < 0)
+        //    {
+        //        currentClipIndex = videoClip.Length - 1; // Vai para o último vídeo se voltar do primeiro
+        //    }
+        //    PlayCurrentVideo();
+        //}
     }
-
-    // Toca o vídeo atual com base no índice
-    void PlayCurrentVideo()
-    {
-        if (currentClipIndex < videoClips.Length && currentClipIndex >= 0)
-        {
-            videoPlayer.clip = videoClips[currentClipIndex];
-            videoPlayer.Play();
-        }
-        else
-        {
-            Debug.Log("Índice fora do limite do array de vídeos.");
-        }
-    }
-
-    // Troca para o próximo vídeo
-    void PlayNextVideo()
-    {
-        currentClipIndex++;
-        if (currentClipIndex >= videoClips.Length)
-        {
-            currentClipIndex = 0; // Reinicia ao primeiro vídeo se passar do último
-        }
-        PlayCurrentVideo();
-    }
-
-    // Troca para o vídeo anterior
-    void PlayPreviousVideo()
-    {
-        currentClipIndex--;
-        if (currentClipIndex < 0)
-        {
-            currentClipIndex = videoClips.Length - 1; // Vai para o último vídeo se voltar do primeiro
-        }
-        PlayCurrentVideo();
-    }
-}
